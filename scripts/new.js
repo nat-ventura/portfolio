c64192f793f9b310588167c43e9b0f26f5bde972
Natalie Villasana <navillasa@gmail.com>
2017-08-28 13:29:44 -0400

    adds woman and computers article, adds bolding for skill-item click-- but it doesn't work if you select items on different rows

--- scripts/new.js
+++ scripts/new.js
@@ -1,3 +1,29 @@
+function toggle(elementID) {
+  var elementCSS = $(elementID).css('display');
+  console.log(elementCSS);
+  if (elementCSS == 'none') {
+      $(elementID).css('display', 'block')
+  } else {
+      $(elementID).css('display', 'none')
+  }
+}
+
+$(document).ready( () => {
+  // for (var i in skills) {
+    
+    // $('.skill-item').on('click', (event) => {
+    //   var select = event.currentTarget.children[1].textContent
+    //   return '<b>' + select + '</b>';
+    // });
+
+    $('.skill-item').click(function (item) {
+      $(this).parent('.skills-box').children('.row').css('font-weight', 'normal');
+      $(this).siblings('.skill-item').css('font-weight', 'normal');
+      $(this).css('font-weight', 'bold');
+    });
+  // };
+});
+
 new jBox('Tooltip', {
     attach: '.tooltip',
     width: 200,
@@ -7,9 +33,9 @@ new jBox('Tooltip', {
       y: 'bottom'
     },
     outside: 'y',
-    // pointer: 'bottom:500',
-    // offset: '5',
-    // pointTo: 'target',
     animation: 'move',
-    closeOnClick: true
+    closeOnClick: true,
+    // blockScroll: 'false'
+    isolateScroll: false,
+    blockScroll: false
 });
