0f6e4152761c89217b8772095147c7e8cf7d9a10
Natalie Villasana <navillasa@gmail.com>
2017-08-29 03:10:53 -0400

    adds favicon, removes blue dividers, creates more minimalist style

--- scripts/new.js
+++ scripts/new.js
@@ -7,7 +7,7 @@
 
 $(document).ready( () => {
 
-    $('.skill-item').click(function (item) {
+    $('.skill-item').mouseover( function (item) {
       // $(this).parent('.skills-box').children('.row').css('font-weight', 'normal');
       $('.skill-item').css('font-weight', 'normal');
       $(this).css('font-weight', 'bold');
@@ -17,26 +17,41 @@ $(document).ready( () => {
       var filtered = $('.flower').css('filter')
       if (filtered == 'none') {
         $('.flower').css('filter', 'invert(1)')
-        // $('.white-screen').css('background-color', 'cyan')
+        $('.white-screen').css('height', '100%')
+        // $('.white-screen').css('background-color', 'rgba(67, 245, 255, 0.91)');
+
+        ;
       }
       if (filtered == 'invert(1)') {
         $('.flower').css('filter', 'none')
+        $('.white-screen').css('height', '110%');
         // $('.flower').css('height', '40rem')
+        // $('.white-screen').css('background-color', 'rgba(255,255,255,.9');
+        // $('.white-screen').css('background-color','rgba(255, 162, 219, 0.91)');
       }
     });
 });
 
+// 
+
+// 
+
+// $(window).scroll(function() {
+//   var scrolledY = $(window).scrollTop();
+//   $('.blue-divider').css('background-position', 'left ' + ((scrolledY)) + 'px');
+// });
+
 new jBox('Tooltip', {
     attach: '.tooltip',
     width: 200,
-    trigger: 'click',
+    trigger: 'mouseenter',
     position: {
       x: 'center',
       y: 'bottom'
     },
     outside: 'y',
     animation: 'move',
-    closeOnClick: true,
+    closeOnClick: false,
     // blockScroll: 'false'
     isolateScroll: false,
     blockScroll: false
