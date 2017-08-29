1fd928ca4bf3d7112e5d2c543846ec9cec82ec13
Natalie Villasana <navillasa@gmail.com>
2017-08-29 12:16:57 -0400

    removes clickable top cloud, aligns writing titles with writing-content-wrapper

--- scripts/new.js
+++ scripts/new.js
@@ -12,25 +12,28 @@ $(document).ready( () => {
       $('.skill-item').css('font-weight', 'normal');
       $(this).css('font-weight', 'bold');
     });
+  });
 
-    $('.flower-container').click( () => {
-      var filtered = $('.flower').css('filter')
-      if (filtered == 'none') {
-        $('.flower').css('filter', 'invert(1)')
-        $('.white-screen').css('height', '100%')
+    // $('.flower-container').click( () => {
+    //   var filtered = $('.flower').css('filter')
+    //   if (filtered == 'none') {
+    //     $('.flower').css('filter', 'invert(1)')
+        // $('.top-name').css('padding-top', '62%')
+        // $('.white-screen').css('height', '100%')
         // $('.white-screen').css('background-color', 'rgba(67, 245, 255, 0.91)');
 
-        ;
-      }
-      if (filtered == 'invert(1)') {
-        $('.flower').css('filter', 'none')
-        $('.white-screen').css('height', '110%');
+      //   ;
+      // }
+      // if (filtered == 'invert(1)') {
+      //   $('.flower').css('filter', 'none')
+      //   $('.white-screen').css('height', '110%');
         // $('.flower').css('height', '40rem')
+        // $('.top-name').css('padding-top', '68%')
         // $('.white-screen').css('background-color', 'rgba(255,255,255,.9');
         // $('.white-screen').css('background-color','rgba(255, 162, 219, 0.91)');
-      }
-    });
-});
+//       }
+//     });
+
 
 // 
 
