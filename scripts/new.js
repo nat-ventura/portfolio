7615c4dac7784073c9fa9560d030ab148569fee0
Natalie Villasana <navillasa@gmail.com>
2017-10-03 23:26:51 -0400

    removes dead code from new.js

--- scripts/new.js
+++ scripts/new.js
@@ -8,13 +8,11 @@
 $(document).ready( () => {
 
     $('.skill-item').mouseover( function (item) {
-      // $(this).parent('.skills-box').children('.row').css('font-weight', 'normal');
       $('.skill-item').css('font-weight', 'normal');
       $(this).css('font-weight', 'bold');
     });
 
     $('.mini-skill-item').mouseover( function (item) {
-      // $('.skill-item p').css('opacity', '0');
       $(this).css('opacity', '1');
     })
 
@@ -50,47 +48,9 @@ $(document).ready( () => {
         if ($('.nav').css('top') != '0px') {
           $('.nav-title').css('display', 'none');
         }
-        // if  {
-        //   $('.nav-title').css('display', 'none');
-        // }
-
+      });
     });
 });
-
-    // $('.contact-item').mouseover ( function (item) {
-    //   $(this).css('opacity', '1');
-    // });
-  });
-
-    // $('.flower-container').click( () => {
-    //   var filtered = $('.flower').css('filter')
-    //   if (filtered == 'none') {
-    //     $('.flower').css('filter', 'invert(1)')
-        // $('.top-name').css('padding-top', '62%')
-        // $('.white-screen').css('height', '100%')
-        // $('.white-screen').css('background-color', 'rgba(67, 245, 255, 0.91)');
-
-      //   ;
-      // }
-      // if (filtered == 'invert(1)') {
-      //   $('.flower').css('filter', 'none')
-      //   $('.white-screen').css('height', '110%');
-        // $('.flower').css('height', '40rem')
-        // $('.top-name').css('padding-top', '68%')
-        // $('.white-screen').css('background-color', 'rgba(255,255,255,.9');
-        // $('.white-screen').css('background-color','rgba(255, 162, 219, 0.91)');
-//       }
-//     });
-
-
-// 
-
-// 
-
-// $(window).scroll(function() {
-//   var scrolledY = $(window).scrollTop();
-//   $('.blue-divider').css('background-position', 'left ' + ((scrolledY)) + 'px');
-// });
 
 new jBox('Tooltip', {
     attach: '.tooltip',
@@ -103,7 +63,6 @@ new jBox('Tooltip', {
     outside: 'y',
     animation: 'move',
     closeOnClick: false,
-    // blockScroll: 'false'
     isolateScroll: false,
     blockScroll: false
 });
